de84eef2e209eb9cfea26aaf1460a83cb3b7f6fd
Tulio-HSG <tuliohsg2004@gmail.com>
2026-06-04 19:00:56 -0300

    feat: cria rotas da api para criação de relatorios, dashboards e exportação dos mesmos em csv e pdf

--- backend/ecommerce/src/server.js
+++ backend/ecommerce/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import reportsRoutes from './modules/reports/reports.routes.js';
+import { errorMiddleware } from './middlewares/error.middleware.js';
 
 dotenv.config();
 
@@ -9,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/reports', reportsRoutes);
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -16,6 +20,8 @@ app.get('/', (req, res) => {
         message: "Explosion API está funcionando!"
     })
 })
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
